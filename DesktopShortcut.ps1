6f026113ff2d88d853f6466252be09a23c41b869
InspiringCode25 <systems@inspireculture.org.uk>
2025-08-22 15:34:54 +0100

    Update DesktopShortcut.ps1

--- DesktopShortcut.ps1
+++ DesktopShortcut.ps1
@@ -1,6 +1,20 @@
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Notepad++.lnk")
-$Shortcut.TargetPath="C:\Program Files\Notepad++\notepad++.exe"
+$Shortcut = $WshShell.CreateShortcut("C:\Users\Public\Desktop\Log Out.lnk")
+$Shortcut.TargetPath="C:\Program Files\netloan\Client\NLClientTS.exe" -logout"
 $Shortcut.Arguments="/l"
-$Shortcut.IconLocation="C:\Program Files\Notepad++\notepad++.exe"
+$shortcut.IconLocation = "shell32.dll,28"
+$Shortcut.Save()
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Log Out.lnk")
+$Shortcut.TargetPath="C:\Program Files\netloan\Client\NLClientTS.exe" -logout"
+$Shortcut.Arguments="/l"
+$shortcut.IconLocation = "shell32.dll,28"
+$Shortcut.Save()
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Lock.lnk")
+$Shortcut.TargetPath="C:\Program Files\netloan\Client\NLClientTS.exe" -lock"
+$Shortcut.Arguments="/l"
+$shortcut.IconLocation = "shell32.dll,48"
 $Shortcut.Save()
